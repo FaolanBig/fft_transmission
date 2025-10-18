ab0c759bcb54c0c641010787bd1c4f3c1b3af3d9
FaolanBig <Faolan.Big@web.de>
2025-10-18 15:22:44 +0200

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace fft_transmission
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
